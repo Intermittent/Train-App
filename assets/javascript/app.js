a878c171d62d7ba324c7db5beda3ad2722b0a24c
Andy Medina <Andy Medina>
2017-06-25 19:17:52 -0600

    Layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+  
+
+  var config = {
+    apiKey: "AIzaSyB9tRG6ZvfBnhOzK4tQY_TpnVkf6qfhsRg",
+    authDomain: "i-like-trains-fc495.firebaseapp.com",
+    databaseURL: "https://i-like-trains-fc495.firebaseio.com",
+    projectId: "i-like-trains-fc495",
+    storageBucket: "",
+    messagingSenderId: "488892627212"
+  };
+  firebase.initializeApp(config);
